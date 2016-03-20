09242e6883196326f06a2c16d0552f47d5656ed2
Matthew Goodwin <matthewjgoodwin@gmail.com>
2016-03-21 02:02:13 +0900

    this is the latest version of the arcade game 3-21-16

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# p4ArcadeGame
-this is the udacity project 4 arcade game
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+
+for self-checking their submission.
+
+
+Engine.js
+Delta Time or Delta Timing is a concept used amongst programmers in relation to hardware and network responsiveness. In graphics programming, the term is usually used for variably updating scenery based on the elapsed time since the game last updated, and also calculated separately if graphics are being multi-threaded.
---
+++ js/app.js
@@ -0,0 +1,103 @@
+// Enemies our player must avoid
+var Enemy = function(haterX, haterY,haterSpeed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x= haterX;
+    this.y= haterY;
+    this.speed= haterSpeed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //--->multiply movements below
+    this.x+= this.speed* dt;
+    if(this.x>520){this.x= -50};
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+//-->below is my player class with update,render and handle methods as protototypes...
+var Player = function(playerX, playerY, playerSpeed){
+    this.x= playerX;
+    this.y=playerY;
+    this.speed= playerSpeed;
+    this.sprite='images/char-boy.png';
+};
+Player.prototype.update= function(dt){this.x= this.x; this.y= this.y;};
+Player.prototype.render= function(){ctx.drawImage(Resources.get(this.sprite), this.x, this.y);};
+Player.prototype.handleInput= function(allowedKeys){
+    switch(allowedKeys){
+        case "up":
+        if(this.y>0){
+        this.y-= 80;//-80 on the y-axis sends the character 80px up towards 0
+        };
+        break;
+
+        case "down":
+       // if(this.y<500){ // if "Y" is less than 500px, then add 80px(which sends the charater downwards)
+        this.y= this.y + 80; //}; 
+        break;
+
+        case "left":
+        if(this.x>0){
+        this.x= this.x - 100;
+        };
+        break;
+
+        case "right":
+        this.x+= 100;
+        break;
+    };
+};
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+//allEnemies array below:
+var hater1= new Enemy(-100,220,20); //enemy y-axis diff= 80px
+var hater2= new Enemy(-100,220,150);
+var hater3= new Enemy(-100,140,70);
+var hater4= new Enemy(-100,60,100);
+var hater5= new Enemy(-100,60,35);
+
+
+var allEnemies= [hater1, hater2, hater3, hater4, hater5];
+
+// Place the player object in a variable called player
+//---=>below is the instance of the player object
+var player = new Player(200,390,100);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+/*document.addEventListener('keyup', playerMove());
+var playerMove= function(){
+    37: 'left';
+    38: 'up';
+    39: 'right';
+    40: 'down';
+};*/
