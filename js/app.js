4d209e6bac8c00c814e0493adad5752c1d30fd1c
Matthew Goodwin <matthewjgoodwin@gmail.com>
2016-04-11 23:57:59 +0900

    trying to setTimout function, but it is not working

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     //--->multiply movements below
     this.x+= this.speed* dt;
-    if(this.x>520){this.x= -50};
+    if(this.x>520){
+        this.x= -50;
+    };
 
 };
 
@@ -45,6 +47,8 @@ Player.prototype.update= function(dt){this.x= this.x; this.y= this.y;
     /*if(this.y<15){
         this.resetGame();
     };*/
+    this.goal();//calls line 55 to init resetGame();
+
 };
 Player.prototype.render= function(){ctx.drawImage(Resources.get(this.sprite), this.x, this.y);};
 Player.prototype.resetGame= function(){this.x= 200; this.y= 390; this.speed= 100;};
@@ -91,12 +95,12 @@ Player.prototype.collisionCheck= function(enemy){
 
     //console.log(e);
     //console.log(this);
-//for (i=0; i < e.length; i++){ // standard loop; as long as i is less than the number of enemies; loops through a number of times
+for (i=0; i < e.length; i++){ // standard loop; as long as i is less than the number of enemies; loops through a number of times
 
 //var eLong = e.length; //expresses enemny array lenght as a varaiable.(part1)
 //for(i=0; i<eLong; i++){}; //then uses variable elong to loop throught the enemny array(part2)
 
-for(var i in e){ //for in loop; loops through allEnemies; loops through properties of an object
+//for(var i in e){ //for in loop; loops through allEnemies; loops through properties of an object
     //console.log(e[i]);
 
 
@@ -105,14 +109,14 @@ for(var i in e){ //for in loop; loops through allEnemies; loops through properti
         this.y< e[i].y+ e[i].length &&
         this.y+this.height>e[i].y){
         //collision detected
-    alert("you lose");
-    this.resetGame();// player.resetGame();
+
+    //alert("you lose");
+    setTimeout(player.resetGame(),10000);
+    //this.resetGame();// player.resetGame();
     //run a console.log of e[i] to ensure that individual array objects are being targeted
         };//ends if statement 
     };//ends for loop
 };//ends prototype function
-
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -143,11 +147,8 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-player.goal();
-/*document.addEventListener('keyup', playerMove());
-var playerMove= function(){
-    37: 'left';
-    38: 'up';
-    39: 'right';
-    40: 'down';
+/*var delayReset= function(){
+    var timeDelay= window.setTimeout(this.resetGame, alert ,10000);
 };*/
+
+
